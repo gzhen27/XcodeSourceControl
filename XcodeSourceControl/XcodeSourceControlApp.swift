cb7601601ba6be5922d78842fb040f95ab65b948
Gray.Z <grayzprogrammer@gmail.com>
2024-10-19 11:45:35 -0500

    Initial Commit

---
+++ XcodeSourceControl/XcodeSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeSourceControlApp.swift
+//  XcodeSourceControl
+//
+//  Created by G Zhen on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct XcodeSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
