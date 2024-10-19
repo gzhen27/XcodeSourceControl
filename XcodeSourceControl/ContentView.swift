91f495ad09b392b597660c07171375eac699c4b9
Gray.Z <grayzprogrammer@gmail.com>
2024-10-19 17:51:33 -0500

    [GitHub Desktop] add paddings on image

--- XcodeSourceControl/ContentView.swift
+++ XcodeSourceControl/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+                .padding()
             Text("Hello, world!!")
                 .padding(.vertical, 20)
         }
