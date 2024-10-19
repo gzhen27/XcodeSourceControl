fda1f5a7db191d5a00c20ff67f24f57bb77f36a9
Gray.Z <grayzprogrammer@gmail.com>
2024-10-19 13:27:39 -0500

    add horizontal paddings to Text.

--- XcodeSourceControl/ContentView.swift
+++ XcodeSourceControl/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, human!")
+                .padding(.horizontal, 20)
         }
         .padding()
     }
